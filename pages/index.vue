0dc68a944c1dae23b3ac64247910cce0bdcc18b0
stefano belli <stefano.belli@weroad.it>
2024-04-27 10:33:10 +0200

    chore(scaffolding): nuxt2 basic setup with composition api

---
+++ pages/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <p v-if="fetchState.pending">
+      loading...
+    </p>
+    <p v-else>
+      The episode is <strong>{{ episode.name }}</strong>
+    </p>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref, useFetch } from '@nuxtjs/composition-api'
+
+const episode = ref('')
+const { fetchState } = useFetch(async () => {
+  const response = await fetch('https://rickandmortyapi.com/api/episode/24')
+  episode.value = await response.json()
+})
+</script>

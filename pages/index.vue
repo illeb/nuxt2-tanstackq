e823fdfea19b3c5d4af43e3ffd184f1d65eb6cbf
stefano belli <stefano.belli@weroad.it>
2024-05-12 14:53:06 +0200

    added useQueries

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,26 @@
 <script setup lang="ts">
-import { useQuery } from '@tanstack/vue-query'
+import { useQueries, useQuery } from '@tanstack/vue-query'
 import Field from '~/components/ui/Field.vue'
 import Card from '~/components/ui/Card.vue'
 
 const episodeNumber = ref(24)
 
-const { isLoading, status, isError, data: episode, error } = useQuery({
+const { isFetching, status, isError, data: episode, error } = useQuery({
   queryKey: ['episode', episodeNumber],
   queryFn: ({ queryKey }) => fetch(`https://rickandmortyapi.com/api/episode/${queryKey[1]}`).then(res => res.json()),
+  keepPreviousData: true
 })
+
+const queries = computed(() => episode.value?.characters.map((characterURL: any) => {
+    return {
+      queryKey: ['character', characterURL],
+      queryFn: ({ queryKey }: any) => fetch(queryKey[1]).then(res => res.json()),
+      keepPreviousData: true
+    }
+  }) ?? []
+);
+const charactersQueries = useQueries({ queries })
+
 </script>
 
 <template>
@@ -21,7 +33,7 @@ const { isLoading, status, isError, data: episode, error } = useQuery({
         required
       >
     </Field>
-    <Card class="min-h-40 h-auto mt-10" :loading="isLoading" clickable>
+    <Card class="min-h-40 h-auto mt-10" :loading="isFetching" clickable>
       <div v-if="episode">
         <h5 class="text-xl font-bold">{{ episode.name }}</h5>
         <div class="flex flex-col gap-2 mt-4">
@@ -30,6 +42,16 @@ const { isLoading, status, isError, data: episode, error } = useQuery({
         </div>
       </div>
     </Card>
+
+    <p class="mt-10 text-xl font-bold">Starring:</p>
+    <div class="mt-4 gap-7 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 2xl:grid-cols-6">
+      <Card v-for="characterQ in charactersQueries"  :key="characterQ?.data.id" class="h-56 min-h-56 p-4 flex" :loading="characterQ.isFetching || isFetching">
+        <div class="flex flex-1 flex-col gap-4" v-if="characterQ.data">
+          <h6 class="font-bold">{{ characterQ.data.name }}</h6>
+          <img class="w-full h-full overflow-hidden object-contain" :src="characterQ.data.image" >
+        </div>
+      </Card>
+    </div>
 
   </div>
 </template>
